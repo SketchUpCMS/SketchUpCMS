b525868263d7cc81289a63867044283a15156622
Tai Sakuma <tai.sakuma@gmail.com>
2012-03-20 11:09:35 +0100

    deleted draw_all_20111213_01

--- cms.rb
+++ cms.rb
@@ -30,7 +30,6 @@ def cmsmain
 
   read_xmlfiles
   draw_gratr_20120317_02
-  # draw_all_20111213_01
 
 end
 
@@ -119,108 +118,6 @@ def draw_array graph, arrayToDraw
   puts "Time elapsed #{(end_time - start_time)*1000} milliseconds"
 
 
-end
-
-##____________________________________________________________________________||
-def draw_all_20111213_01
-
-  $solidsManager.clear
-  $logicalPartsManager.clear
-  $posPartsManager.clear
-  Sketchup.active_model.definitions.purge_unused
-
-  $solidsManager.eraseAfterDefine = true
-  $logicalPartsManager.eraseAfterDefine = true
-
-  start_time = Time.now
-  Sketchup.active_model.start_operation("Draw CMS", true)
-
-  def drawChildren lp, depth = 1
-    # return unless lp.isMaterialToHide(lp.materialName)
-    # p "#{lp.name}, #{lp.materialName}"
-    depth -= 1
-    return if depth == -1
-    lp.children.each do |pp|
-      drawChildren(pp.child, depth)
-      next if pp.done
-      begin
-        ## p pp 
-        if depth == 0 or pp.child.children.size == 0 or not (pp.child.materialName.to_s =~ /Air$/ or pp.child.materialName.to_s =~ /free_space$/)
-          pp.child.instantiateSolid()
-        end
-        pp.exec
-      rescue Exception => e
-        puts e.message
-      end
-    end
-  end
-
-  def drawParentUntil lp, pname
-    return if lp.name == pname
-    lp.parents.each do |pp|
-      drawParentUntil pp.parent, pname
-      begin
-        p pp unless pp.done
-        pp.exec
-      rescue Exception => e
-        puts e.message
-      end
-    end
-  end
-
-  
-  lp = $logicalPartsManager.get("muonBase:MBWheel_0".to_sym)
-  drawChildren lp, 9
-
-  # lp = $logicalPartsManager.get("tracker:Tracker".to_sym)
-  # drawChildren lp, 9
-  # drawParentUntil lp, "tracker:Tracker".to_sym
-
-  # lp = $logicalPartsManager.get("caloBase:CALO".to_sym)
-  # drawChildren lp, 10
-
-  # lp = $logicalPartsManager.get("muonBase:MUON".to_sym)
-  # drawChildren lp, 9
-
-  # lp = $logicalPartsManager.get("hcalforwardalgo:VCAL".to_sym)
-  # drawChildren lp, 4
-
-  # lp = $logicalPartsManager.get("forward:TotemT1".to_sym)
-  # drawChildren lp, 3
-  # drawParentUntil lp, "cms:CMSE".to_sym
-  # 
-  # lp = $logicalPartsManager.get("forward:TotemT2".to_sym)
-  # drawChildren lp, 3
-  # drawParentUntil lp, "cms:CMSE".to_sym
-
-  # lp = $logicalPartsManager.get("beampipe:BEAM".to_sym)
-  # drawChildren lp, 5
-  # drawParentUntil lp, "cms:CMSE".to_sym
-  # lp = $logicalPartsManager.get("beampipe:BEAM1".to_sym)
-  # drawChildren lp, 5
-  # drawParentUntil lp, "cms:CMSE".to_sym
-  # lp = $logicalPartsManager.get("beampipe:BEAM2".to_sym)
-  # drawChildren lp, 5
-  # drawParentUntil lp, "cms:CMSE".to_sym
-  # lp = $logicalPartsManager.get("beampipe:BEAM3".to_sym)
-  # drawChildren lp, 5
-  # drawParentUntil lp, "cms:CMSE".to_sym
-
-  # lp = $logicalPartsManager.get("cms:CMSE".to_sym)
-  # drawChildren lp, 13
-
-  definition = lp.definition
-  if definition
-    entities = Sketchup.active_model.entities
-    transform = Geom::Transformation.new(Geom::Point3d.new(0, 0, 15.m))
-    solidInstance = entities.add_instance definition, transform
-  end
-
-  Sketchup.active_model.commit_operation
-  end_time = Time.now
-  puts "Time elapsed #{(end_time - start_time)*1000} milliseconds"
-
-  Sketchup.active_model.active_view.zoom_extents
 end
 
 ##____________________________________________________________________________||
