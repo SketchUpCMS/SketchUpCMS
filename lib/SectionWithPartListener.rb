5d123dc75d4b6b42d465fe5f72e09b78eecf6e67
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-22 18:51:33 -0500

    move several classes from ddlcallbacks_listeners.rb to SectionWithPartListener.rb

---
+++ lib/SectionWithPartListener.rb
@@ -0,0 +1,78 @@
+# Tai Sakuma <sakuma@fnal.gov>
+
+##____________________________________________________________________________||
+class PartListenerDispatcher < ListenerDispatcher
+  attr_accessor :listener
+  def initialize()
+    @isInListen = false
+    @currentTagName = ""
+  end
+  def tag_start(name, attributes, sectionLabel)
+    if (@isInListen)
+      @listener.tag_start(name, attributes)
+      return
+    end
+    @currentTagName = name
+    @isInListen = true
+    @listener.listener_enter(name, attributes, sectionLabel)
+  end
+
+  def tag_end(name)
+    if (@isInListen)
+      if (@currentTagName == name)
+        @listener.listener_exit(name)
+        @isInListen = false
+        @currentTagName = ""
+        return
+      else
+        @listener.tag_end(name)
+        return
+      end
+    end
+  end
+end
+
+##____________________________________________________________________________||
+class PartListener < DDLListener
+  def initialize(geometryManager, sectionaName)
+    super()
+    @geometryManager = geometryManager
+    @sectionaName = sectionaName.to_sym
+  end
+  def listener_enter(name, attributes, sectionLabel)
+    @sectionLabel = sectionLabel
+    @partName = name
+    @args = attributes
+  end
+  def listener_exit(name)
+    super
+    @geometryManager.add_entry(@sectionaName, @sectionLabel, @partName, @args)
+  end
+  def tag_start(name, attributes)
+    @args[name] = Array.new if !(@args.key?(name))
+    @args[name] << attributes
+  end
+end
+
+##____________________________________________________________________________||
+class SectionWithPartListener < DDLListener
+  def initialize(sectionaName, geometryManager)
+    super()
+    @listenersDispatcher = PartListenerDispatcher.new
+    @listenersDispatcher.listener = PartListener.new(geometryManager, sectionaName)
+  end
+  def listener_enter(name, attributes)
+    @sectionLabel = attributes['label'].sub(/\.xml/, '')
+  end
+  def listener_exit(name)
+    @sectionLabel = ''
+  end
+  def tag_start(name, attributes)
+    @listenersDispatcher.tag_start name, attributes, @sectionLabel
+  end
+  def tag_end(name)
+    @listenersDispatcher.tag_end name
+  end
+end
+
+##____________________________________________________________________________||
