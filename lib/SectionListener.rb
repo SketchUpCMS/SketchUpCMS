5211035e068b82d5fd65c853df12f0173b7bc5ee
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-22 19:15:54 -0500

    move the class SectionListener to its own file

---
+++ lib/SectionListener.rb
@@ -0,0 +1,25 @@
+# Tai Sakuma <sakuma@fnal.gov>
+
+##____________________________________________________________________________||
+class SectionListener < DDLListener
+  def initialize(sectionaName, geometryManager)
+    super()
+    @geometryManager = geometryManager
+    @sectionaName = sectionaName.to_sym
+  end
+  def listener_enter(name, attributes)
+    @sectionLabel = attributes['label'].sub(/\.xml/, '')
+  end
+  def listener_exit(name)
+    @sectionLabel = ''
+  end
+  def tag_start(name, attributes)
+    @name = name
+    @attributes = attributes
+  end
+  def tag_end(name)
+    @geometryManager.add_entry(@sectionaName, @sectionLabel, @name, @attributes)
+  end
+end
+
+##____________________________________________________________________________||
--- lib/ddlcallbacks_listeners.rb
+++ lib/ddlcallbacks_listeners.rb
@@ -22,26 +22,5 @@ class NullListener < DDLListener
 end
 
 ##____________________________________________________________________________||
-class SectionListener < DDLListener
-  def initialize(sectionaName, geometryManager)
-    super()
-    @geometryManager = geometryManager
-    @sectionaName = sectionaName.to_sym
-  end
-  def listener_enter(name, attributes)
-    @sectionLabel = attributes['label'].sub(/\.xml/, '')
-  end
-  def listener_exit(name)
-    @sectionLabel = ''
-  end
-  def tag_start(name, attributes)
-    @name = name
-    @attributes = attributes
-  end
-  def tag_end(name)
-    @geometryManager.add_entry(@sectionaName, @sectionLabel, @name, @attributes)
-  end
-end
-
-##____________________________________________________________________________||
+require "SectionListener"
 require "SectionWithPartListener"
