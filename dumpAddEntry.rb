3a885d3f3ee60d7d1959707db963efcd09e64919
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-22 20:37:05 -0500

    add dumpAddEntry.rb

---
+++ dumpAddEntry.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# Tai Sakuma <sakuma@fnal.gov>
+
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)) + "/lib")
+require 'buildDDLCallBacks'
+require 'readXMLFiles'
+
+##____________________________________________________________________________||
+class GeometryManagerDump
+  def xmlFilePath=(v)
+    @xmlFilePath = v
+    @baseName = File::basename(@xmlFilePath).sub(/\.xml/, '').to_sym
+  end
+  def add_entry sectionName, sectionLabel, partName, args
+    print "add_entry\n"
+    p sectionName, sectionLabel, partName, args
+    print "\n"
+  end
+end
+
+
+##____________________________________________________________________________||
+def main
+
+  topDir = File.expand_path(File.dirname(__FILE__)) + '/'
+  xmlfileList = ['GeometryTOB.xml']
+  xmlfileList.map! {|f| f = topDir + f }
+
+  geometryManager = GeometryManagerDump.new
+  callBacks = buildDDLCallBacks(geometryManager)
+  readXMLFiles(xmlfileList, callBacks, geometryManager)
+
+end
+
+##____________________________________________________________________________||
+
+main
