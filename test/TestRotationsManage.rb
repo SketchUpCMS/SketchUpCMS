40ae9e39b5f9697c4d2cffb19d0afe0f776cc351
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-28 17:12:37 -0500

    add TestRotationsManage.rb

---
+++ test/TestRotationsManage.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# Tai Sakuma <sakuma@fnal.gov>
+
+require 'test/unit'
+
+require "RotationsManager"
+  
+##____________________________________________________________________________||
+class TestRotationsManager < Test::Unit::TestCase
+
+  class MockGeometryManager
+  end
+
+  def setup  
+    @geometryManager = MockGeometryManager.new
+    @rotationsManager = RotationsManager.new
+  end
+
+  def test_add
+
+    part = Rotation.new(@geometryManager, :Rotation)
+    part.name = :"DdBlNa:DdBlNa1306"
+
+    @rotationsManager.add part
+
+    assert_equal([part], @rotationsManager.parts)
+    assert_equal({:"DdBlNa:DdBlNa1306" => part}, @rotationsManager.partsHash)
+
+  end
+
+  def test_add_unknown
+
+    part = Rotation.new(@geometryManager, :UnknownRotation)
+    part.name = :"DdBlNa:DdBlNa1306"
+
+    ioerr = StringIO.new
+    $stderr = ioerr
+    @rotationsManager.add part
+    $stderr = STDERR
+    assert_equal("RotationsManager: Unknown part name: \"UnknownRotation\"\n", ioerr.string)
+
+    assert_equal([], @rotationsManager.parts)
+
+  end
+
+end
+
+##____________________________________________________________________________||
