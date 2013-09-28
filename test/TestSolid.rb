858a355dcad45f3dce2fd8ebddab7ff3d7254568
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-28 18:26:34 -0500

    add TestSolid.rb

---
+++ test/TestSolid.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# Tai Sakuma <sakuma@fnal.gov>
+
+require 'test/unit'
+
+require "Solid"
+  
+##____________________________________________________________________________||
+class TestSolid < Test::Unit::TestCase
+
+  class MockGeometryManager
+  end
+
+  def setup  
+    @geometryManager = MockGeometryManager.new
+  end
+
+  def test_one
+    solid = Solid.new(@geometryManager, :Solid)
+  end
+
+end
+
+##____________________________________________________________________________||
