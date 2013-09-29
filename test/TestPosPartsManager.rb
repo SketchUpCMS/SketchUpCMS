c90debf85c3df4f278c68cdedb8a6edfd9cce205
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-29 10:29:26 -0500

    add TestPosPartsManager.rb and TestPosPart.rb

---
+++ test/TestPosPartsManager.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+# Tai Sakuma <sakuma@fnal.gov>
+
+require 'test/unit'
+
+require "PosPartsManager"
+  
+##____________________________________________________________________________||
+class TestPosPartsManager < Test::Unit::TestCase
+
+  class MockGeometryManager
+  end
+
+  def setup  
+    @geometryManager = MockGeometryManager.new
+    @posPartsManager = PosPartsManager.new
+  end
+
+  def test_add
+
+    part = PosPart.new(@geometryManager, :PosPart)
+    part.parentName = :"tobmodule0:TOBModule0"
+    part.childName = :"tobrod1l:TOBRodCentral1L"
+
+    @posPartsManager.add part
+
+    assert_equal([part], @posPartsManager.parts)
+    assert_equal({:"tobmodule0:TOBModule0" => [part]}, @posPartsManager.partsHashByParent)
+    assert_equal({:"tobrod1l:TOBRodCentral1L" => [part]}, @posPartsManager.partsHashByChild)
+    assert_equal({[:"tobmodule0:TOBModule0", :"tobrod1l:TOBRodCentral1L"] => [part]}, @posPartsManager.partsHashByParentChild)
+
+  end
+
+  def test_get
+
+    part = PosPart.new(@geometryManager, :PosPart)
+    part.parentName = :"tobmodule0:TOBModule0"
+    part.childName = :"tobrod1l:TOBRodCentral1L"
+
+    @posPartsManager.add part
+
+    assert_equal([part], @posPartsManager.getByParent(:"tobmodule0:TOBModule0"))
+    assert_equal([part], @posPartsManager.getByChild(:"tobrod1l:TOBRodCentral1L"))
+    assert_equal([part], @posPartsManager.getByParentChild(:"tobmodule0:TOBModule0", :"tobrod1l:TOBRodCentral1L"))
+
+    assert_equal([], @posPartsManager.getByParent(:wrongname))
+    assert_equal([], @posPartsManager.getByChild(:wrongname))
+    assert_equal([], @posPartsManager.getByParentChild(:wrongname, :wrongname))
+
+  end
+
+end
+
+##____________________________________________________________________________||
