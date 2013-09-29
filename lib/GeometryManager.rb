c44ef4d10cffaa9f1716446a55fbab4cfb0f4a80
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-28 19:24:20 -0500

    remove @inDDLs and add attr_reader :entries

--- lib/GeometryManager.rb
+++ lib/GeometryManager.rb
@@ -19,20 +19,18 @@ class GeometryManager
   attr_accessor :materialsManager, :rotationsManager
   attr_accessor :solidsManager, :logicalPartsManager, :posPartsManager
   attr_accessor :partBuilder
-  attr_reader :inDDLs
+  attr_reader :entries
 
   def inspect
     "#<" + self.class.name + ":0x" + self.object_id.to_s(16) + ">"
   end
   def initialize
-    @inDDLs = Array.new
     @entries = Array.new
   end
 
   def add_entry sectionName, sectionLabel, partName, args
     # e.g. :SolidSection, "GeometryExtended", "Polycone", {"ZSection"= ... }
     inDDL = {:partName => partName.to_sym, :sectionLabel => sectionLabel.to_sym, :args => args}
-    @inDDLs << inDDL
     @entries << {:sectionName => sectionName, :inDDL => inDDL}
     add_inDDL_to_sectionManager sectionName, inDDL
   end
