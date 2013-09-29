10aff2770a9054de602a6c6cbb5e5f03b188dd1f
Tai Sakuma <tai.sakuma@gmail.com>
2013-09-29 13:28:41 -0500

    add a check if the partName is known

--- lib/SolidsManager.rb
+++ lib/SolidsManager.rb
@@ -8,6 +8,9 @@ class SolidsManager
   attr_accessor :eraseAfterDefine
   attr_accessor :partsHash, :parts
   attr_accessor :entityDisplayer
+
+  KnownPartNames = [:PseudoTrap, :Trd1, :Polycone, :Polyhedra, :Trapezoid, :Tubs, :Box, :Cone, :Torus, :UnionSolid, :SubtractionSolid]
+
   def inspect
     "#<" + self.class.name + ":0x" + self.object_id.to_s(16) + ">"
   end
@@ -24,6 +27,9 @@ class SolidsManager
     @partsHash.key?(name)? @partsHash[name] : nil
   end
   def add part
+    if ! KnownPartNames.include?(part.partName)
+      $stderr.write self.class.name + ": Unknown part name: \"#{part.partName}\"\n"
+    end
     @parts << part
     @partsHash[part.name] = part 
   end
