cb50cb5db2068a5c5e27c2bf07444b31ea9a8a55
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-22 15:32:35 +0200

    update the ruby version in sitecfg.rb

--- sitecfg.rb
+++ sitecfg.rb
@@ -3,10 +3,10 @@
 
 ##____________________________________________________________________________||
 # to use OSX system Ruby
-$LOAD_PATH.push("/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8")
+$LOAD_PATH.push("/System/Library/Frameworks/Ruby.framework/Versions/2.0/usr/lib/ruby/2.0")
 
 # to use MacPorts Ruby
-# $LOAD_PATH.push("/opt/local/lib/ruby/site_ruby/", "/opt/local/lib/ruby/1.8")
+# $LOAD_PATH.push("/opt/local/lib/ruby/site_ruby/", "/opt/local/lib/ruby/2.0")
 
 ##____________________________________________________________________________||
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
