f6faafaa43f13a51c33fe01a35021e26101c144a
Tai Sakuma <tai.sakuma@gmail.com>
2015-06-05 12:16:31 +0200

    add a new file cms_hcal.rb

---
+++ cms_hcal.rb
@@ -0,0 +1,179 @@
+# Tai Sakuma <sakuma@fnal.gov>
+require 'sketchup'
+
+require File.dirname(__FILE__) + '/sitecfg.rb'
+
+require 'gratr'
+
+require 'buildDDLCallBacks'
+require 'readXMLFiles'
+require 'PartBuilder'
+require 'solids'
+require 'GeometryManager'
+require 'EntityDisplayer.rb'
+require 'RotationsManager.rb'
+require 'SolidsManager.rb'
+require 'LogicalPartsManager.rb'
+require 'PosPartsManager.rb'
+require 'MaterialsManager.rb'
+require 'defs.rb'
+
+##____________________________________________________________________________||
+def cmsmain
+
+  read_xmlfiles
+  # read_xmlfiles_from_cache
+
+  draw_gratr_20120317_02
+
+end
+
+##____________________________________________________________________________||
+def draw_gratr_20120317_02
+  def create_array_to_draw graph, topName
+
+
+    # GRATR::Digraph
+    graphFromCMSE = subgraph_from(graph, topName)
+
+    nameDepthHB = [ {:name => :"hcalbarrelalgo:HB", :depth => 0}, ]
+
+    nameDepthList = nameDepthHB
+
+    names = nameDepthList.collect { |e| e[:name] }
+    graphFromCMSEToNames = subgraph_from_to(graphFromCMSE, topName, names)
+
+    graphFromNames = GRATR::Digraph.new
+    nameDepthList.each do |e|
+      graphFromNames = graphFromNames + subgraph_from_depth(graphFromCMSE, e[:name], e[:depth])
+    end
+
+    graphToDraw = graphFromCMSEToNames + graphFromNames
+
+    # e.g. [:"cms:CMSE", :"tracker:Tracker", :"tob:TOB", .. ]
+    toDrawNames = graphToDraw.size > 0 ? graphToDraw.topsort(topName) : [topName]
+
+    toDrawNames
+  end
+
+  # all PosParts in the XML file
+  graphAll = GRATR::Digraph.new
+  $posPartsManager.parts.each { |pp| graphAll.add_edge!(pp.parentName, pp.childName) }
+
+  topName = :"cms:CMSE"
+  arrayToDraw = create_array_to_draw graphAll, topName
+  draw_array graphAll, arrayToDraw.reverse, topName
+end
+
+##____________________________________________________________________________||
+def draw_array graph, arrayToDraw, topName
+
+  Sketchup.active_model.definitions.purge_unused
+  start_time = Time.now
+  Sketchup.active_model.start_operation("Draw CMS", true)
+
+  arrayToDraw.each do |parent|
+    children = graph.neighborhood(parent, :out)
+    children = children.select { |e| arrayToDraw.include?(e) }
+    children.each do |child|
+      posParts = $posPartsManager.getByParentChild(parent, child)
+      posParts.each do |posPart|
+        # puts "  exec posPart #{posPart.parentName} - #{posPart.childName}"
+        posPart.exec
+      end
+    end
+  end
+
+  arrayToDraw.each do |v|
+    logicalPart = $logicalPartsManager.get(v)
+    next if logicalPart.children.size > 0 and logicalPart.materialName.to_s =~ /Air$/
+    next if logicalPart.children.size > 0 and logicalPart.materialName.to_s =~ /free_space$/
+    logicalPart.placeSolid()
+  end
+
+  $logicalPartsManager.get(topName).placeSolid()
+
+  arrayToDraw.each do |v|
+    logicalPart = $logicalPartsManager.get(v)
+    logicalPart.define()
+  end
+
+  lp = $logicalPartsManager.get(topName.to_sym)
+  definition = lp.definition
+  if definition
+    entities = Sketchup.active_model.entities
+    transform = Geom::Transformation.new(Geom::Point3d.new(0, 0, 15.m))
+    solidInstance = entities.add_instance definition, transform
+  end
+
+  Sketchup.active_model.commit_operation
+  end_time = Time.now
+  puts "Time elapsed #{(end_time - start_time)*1000} milliseconds"
+
+
+end
+
+##____________________________________________________________________________||
+def read_xmlfiles
+  topDir = File.expand_path(File.dirname(__FILE__)) + '/'
+  xmlfileListTest = [
+                     'GeometryExtended.xml'
+                    ]
+
+  xmlfileList = xmlfileListTest
+
+  xmlfileList.map! {|f| f = topDir + f }
+
+  p xmlfileList
+
+  geometryManager = buildGeometryManager()
+  callBacks = buildDDLCallBacks(geometryManager)
+  readXMLFiles(xmlfileList, callBacks, geometryManager)
+end
+
+##____________________________________________________________________________||
+def read_xmlfiles_from_cache
+  fillGeometryManager($geometryManager)
+  $geometryManager.reload_from_cache
+end
+
+##____________________________________________________________________________||
+def fillGeometryManager(geometryManager)
+
+  $materialsManager = MaterialsManager.new
+  $rotationsManager = RotationsManager.new
+  $solidsManager = SolidsManager.new
+  $logicalPartsManager = LogicalPartsManager.new
+  $posPartsManager = PosPartsManager.new
+
+  geometryManager.partBuilder = PartBuilder.new
+
+  $solidsManager.entityDisplayer = EntityDisplayer.new('solids', 100.m, 0, 0)
+  $logicalPartsManager.entityDisplayer = EntityDisplayer.new('logicalParts', -100.m, 0, 0)
+
+  geometryManager = geometryManager
+  geometryManager.materialsManager = $materialsManager
+  geometryManager.rotationsManager = $rotationsManager
+  geometryManager.solidsManager = $solidsManager
+  geometryManager.logicalPartsManager = $logicalPartsManager
+  geometryManager.posPartsManager = $posPartsManager
+
+  $materialsManager.geometryManager = geometryManager
+  $rotationsManager.geometryManager = geometryManager
+  $solidsManager.geometryManager = geometryManager
+  $logicalPartsManager.geometryManager = geometryManager
+  $posPartsManager.geometryManager = geometryManager
+
+  geometryManager
+end
+
+##____________________________________________________________________________||
+def buildGeometryManager
+  $geometryManager = GeometryManager.new
+  fillGeometryManager($geometryManager)
+  $geometryManager
+end
+
+##____________________________________________________________________________||
+
+cmsmain
