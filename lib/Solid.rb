50f28d704ec1a756e672f8e0a2c916d2f6af92a4
Tai Sakuma <tai.sakuma@gmail.com>
2015-12-21 09:39:38 +0000

    update comments in Solid

--- lib/Solid.rb
+++ lib/Solid.rb
@@ -2,19 +2,41 @@
 
 require 'SolidDefiner'
 
-##____________________________________________________________________________||
+##__________________________________________________________________||
 class Solid
   attr_accessor :geometryManager
   attr_accessor :name
   attr_accessor :partName
   attr_accessor :argsInDDL
+
+  # example, basic solid
+  #   name = :"tob:TOBAxService_8C"
+  #   partName = :Tubs
+  #   argsInDDL = {
+  #     "dz"=>"830.00*mm", "deltaPhi"=>"9.00*deg",
+  #     "rMax"=>"1169.00*mm", "rMin"=>"1160.00*mm",
+  #     "startPhi"=>"141.00*deg",
+  #     "name"=>"tob:TOBAxService_8C"}
+  #
+  # example, compound solid
+  #   name = :"mgnt:MGNT"
+  #   partName = :SubtractionSolid
+  #   argsInDDL = {
+  #     "rRotation"=>[{"name"=>"rotations:RMCHIMHOLEN"}],
+  #     "rSolid"=>[{"name"=>"mgnt:MGNT_1"}, {"name"=>"mgnt:CHIMNEY_HOLE_N"}],
+  #     "Translation"=>[{"z"=>"-1560.00*mm", "y"=>"2931.495991810324994731*mm", "x"=>"1692.499999999999772626*mm"}],
+  #     "name"=>"mgnt:MGNT"}
+  #
+
   def inspect
     "#<#{self.class.name}:0x#{self.object_id.to_s(16)} #{@name}>"
   end
+
   def initialize geometryManager, partName
     @geometryManager = geometryManager
     @partName = partName
   end
+
   def definition
     return @definition if (@definition and (not @definition.deleted?))
     @definition = defineSolid()
@@ -34,4 +56,4 @@ class Solid
 
 end
 
-##____________________________________________________________________________||
+##__________________________________________________________________||
