c929596c802ca34f0f489628d57a3404bc6496ef
Tai Sakuma <tai.sakuma@gmail.com>
2015-12-23 19:19:48 +0000

    add comments to subgraph_from_depth()

--- lib/graph_functions.rb
+++ lib/graph_functions.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # Tai Sakuma <sakuma@fnal.gov>
 
-
 ##__________________________________________________________________||
 # Returns a subgraph of `graph` which starts from `from`.
 def subgraph_from(graph, from)
@@ -45,17 +44,54 @@ def subgraph_from(graph, from)
 end
 
 ##__________________________________________________________________||
+# Returns a subgraph of `graph` which starts from `from` and has a
+# depth of `depth`.
 def subgraph_from_depth(graph, from, depth = -1)
 
+  # e.g.,
+  # graph = GRATR::DirectedPseudoGraph[0,1, 1,2, 1,2, 1,3, 2,4, 2,5,
+  #                                    2,5, 3,6, 3,7, 5,8, 6,8, 6,9]
+  #            0
+  #            |
+  #            1
+  #         //    \
+  #        2       3
+  #       / \\    / \
+  #      4   5   6   7
+  #           \ /\
+  #            8  9
+  #
+  # from = 1
+  # depth = 2
+
   graphFrom = subgraph_from(graph, from)
+  #            1
+  #         //    \
+  #        2       3
+  #       / \\    / \
+  #      4   5   6   7
+  #           \ /\
+  #            8  9
+  #
+
   return graphFrom if depth < 0
 
   simple_weight = Proc.new {|e| 1}
   distance, path = graphFrom.shortest_path(from, simple_weight)
+  # e.g., depth = {1=>0, 3=>1, 7=>2, 6=>2, 9=>3, 2=>1, 5=>2, 8=>3, 4=>2}
+  #       path = {3=>1, 7=>3, 6=>3, 9=>6, 2=>1, 5=>2, 8=>5, 4=>2}
 
-  graphFromDepth = graph.class.new
-  graphFrom.edges.each { |a| graphFromDepth.add_edge!(a) if distance[a.target] <= depth }
-  graphFromDepth
+  ret = graph.class.new
+  graphFrom.edges.each { |a| ret.add_edge!(a) if distance[a.target] <= depth }
+
+  #
+  #            1
+  #         //    \
+  #        2       3
+  #       / \\    / \
+  #      4   5   6   7
+  #
+  ret
 end
 
 ##__________________________________________________________________||
