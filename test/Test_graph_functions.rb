d8f923d6d4bd58314b370552d677bf3f03251312
Tai Sakuma <tai.sakuma@gmail.com>
2015-12-23 18:05:18 +0000

    add a sample graph in Test_graph_functions

--- test/Test_graph_functions.rb
+++ test/Test_graph_functions.rb
@@ -36,6 +36,24 @@ class Test_graph_functions < Test::Unit::TestCase
     #           /    \
     #          10     11
 
+
+    @graph_1 = GRATR::DirectedPseudoGraph[0,1,
+                                          1,2, 1,2, 1,3,
+                                          2,4, 2,5, 2,5,
+                                          3,6, 3,7,
+                                          5,8,
+                                          6,8, 6,9,
+                                         ]
+    #            0
+    #            |
+    #            1
+    #         //    \
+    #        2       3
+    #       / \\    / \
+    #      4   5   6   7
+    #           \ /\   
+    #            8  9
+
   end
 
   def test_basics
