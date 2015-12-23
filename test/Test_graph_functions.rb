71f81bba2de28a986704fb5d96258aa1e7e400da
Tai Sakuma <tai.sakuma@gmail.com>
2015-12-23 10:29:29 +0000

    rename TestGRATRdefs Test_graph_functions

--- test/Test_graph_functions.rb
+++ test/Test_graph_functions.rb
@@ -8,7 +8,7 @@ require "stringio"
 require "graph_functions"
 
 ##__________________________________________________________________||
-class TestGRATRdefs < Test::Unit::TestCase
+class Test_graph_functions < Test::Unit::TestCase
 
   def setup
     @graph = GRATR::DirectedPseudoGraph[1,2, 1,2, 1,3, 2,4, 2,4, 2,4, 2,5, 3,6, 3,7, 3,7]
