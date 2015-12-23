8d8bf748b58c5c59c6120883a2b6f409984ef9d1
Tai Sakuma <tai.sakuma@gmail.com>
2015-12-23 13:35:13 +0000

    clean up the code in  Test_graph_functions

--- test/Test_graph_functions.rb
+++ test/Test_graph_functions.rb
@@ -4,91 +4,93 @@
 $LOAD_PATH.push(File.join(File.dirname(File.dirname(__FILE__)), 'gratr', 'lib'))
 
 require 'test/unit'
-require "stringio"
 require "graph_functions"
 
 require 'gratr'
+require 'gratr/dot'
 
 ##__________________________________________________________________||
 class Test_graph_functions < Test::Unit::TestCase
 
   def setup
-    @graph = GRATR::DirectedPseudoGraph[1,2, 1,2, 1,3, 2,4, 2,4, 2,4, 2,5, 3,6, 3,7, 3,7]
-    #            1
-    #         //    \
-    #        2       3
-    #     /// \     / \\
-    #      4   5   6   7
-
-    @graph2 = GRATR::DirectedPseudoGraph[0,1, 1,2, 1,2, 1,3, 2,4, 2,4, 2,4, 2,5, 3,6, 3,7, 3,7, 5,8, 5,8, 6,8, 6,8]
+    @graph_0 = GRATR::DirectedPseudoGraph[0,1,
+                                          1,2, 1,2, 1,3,
+                                          2,4, 2,4, 2,5, 2,5, 2,5,
+                                          3,6, 3,7, 3,7, 3,12, 3,12,
+                                          5,8, 5,8,
+                                          6,8, 6,8, 6,9,
+                                          8,10,
+                                          9,11,
+                                          7,13,
+                                          12,13
+                                         ]
     #            0
     #            |
     #            1
-    #         //    \
-    #        2       3
-    #     /// \     / \\
-    #      4   5   6   7
-    #          \\//
-    #            8
-
-    @graph3 = GRATR::DirectedPseudoGraph[1,2, 1,2, 1,3, 2,4, 2,4, 2,4, 2,5, 3,6, 3,7, 3,7, 5,8, 5,8, 6,8, 6,8, 6,9, 8,10, 9,11]
-    #            1
-    #         //    \
-    #        2       3
-    #     /// \     / \\
-    #      4   5   6   7
-    #          \\//\
-    #            8  9
+    #         //     \
+    #        2         3
+    #      //\\\    /  ||  \\
+    #      4   5   6   7    12
+    #          \\//\    \  /
+    #            8  9    13
     #           /    \
     #          10     11
+
   end
 
   def test_basics
-    assert_equal 10, @graph.edges.size
-    assert_equal 7, @graph.size
-    assert_equal 10, @graph.num_edges
-    assert_equal 7, @graph.num_vertices
-    assert @graph.oriented?
+    # @graph_0.write_to_graphic_file('jpg','visualize')
+    assert_equal 23, @graph_0.edges.size
+    assert_equal 14, @graph_0.size
+    assert_equal 23, @graph_0.num_edges
+    assert_equal 14, @graph_0.num_vertices
+    assert @graph_0.oriented?
   end
 
   def test_subgraph_from
-    sub = subgraph_from(@graph, 2)
+    sub = subgraph_from(@graph_0, 2)
     #        2
-    #     /// \
+    #      //\\\
     #      4   5
-    assert_equal @graph.class, sub.class
-    assert_equal [2, 4, 5], sub.vertices.sort
-    assert_equal 4, sub.num_edges
+    #          \\
+    #            8
+    #           /
+    #          10
+    assert_equal @graph_0.class, sub.class
+    assert_equal [2, 4, 5, 8, 10], sub.vertices.sort
+    assert_equal 8, sub.num_edges
     assert sub.edge?(2,4)
     assert sub.edge?(2,5)
+    assert sub.edge?(5,8)
+    assert sub.edge?(8,10)
   end
 
   def test_subgraph_from_to
-    sub = subgraph_from_to(@graph2, 0, [8])
+    sub = subgraph_from_to(@graph_0, 0, [8])
     #            0
     #            |
     #            1
     #         //    \
     #        2       3
-    #         \     /
+    #        \\\    /
     #          5   6
     #          \\//
     #            8
-    assert_equal @graph2.class, sub.class
+    assert_equal @graph_0.class, sub.class
     assert_equal [0, 1, 2, 3, 5, 6, 8], sub.vertices.sort
-    assert_equal 10, sub.num_edges
+    assert_equal 12, sub.num_edges
   end
 
   def test_subgraph_from_depth
-    sub = subgraph_from_depth(@graph3, 3, 2)
-    #                3
-    #               / \\
-    #              6   7
-    #            //\
-    #            8  9
-    assert_equal @graph3.class, sub.class
-    assert_equal [3, 6, 7, 8, 9], sub.vertices.sort
-    assert_equal 6, sub.num_edges
+    sub = subgraph_from_depth(@graph_0, 3, 2)
+    #                  3
+    #               /  ||  \\
+    #              6   7    12
+    #            //\    \  /
+    #            8  9    13
+    assert_equal @graph_0.class, sub.class
+    assert_equal [3, 6, 7, 8, 9, 12, 13], sub.vertices.sort
+    assert_equal 10, sub.num_edges
   end
 
 end
