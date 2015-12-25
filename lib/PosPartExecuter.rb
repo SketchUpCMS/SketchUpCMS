8a146e9db640e9b46d7d5a7c992134168f76ade0
Tai Sakuma <tai.sakuma@gmail.com>
2015-12-25 20:25:40 +0000

    add a new class PosPartExecuter

---
+++ lib/PosPartExecuter.rb
@@ -0,0 +1,21 @@
+# Tai Sakuma <sakuma@fnal.gov>
+
+##__________________________________________________________________||
+class PosPartExecuter
+
+  def initialize
+    @doneList = Set.new
+  end
+
+  def exec posPart
+    return if @doneList.include? posPart
+    child = posPart.child()
+    return unless child
+    posPart.parent().placeChild(child, posPart.translation(), posPart.rotation())
+    posPart.done = true
+    @doneList.add posPart
+  end
+
+end
+
+##__________________________________________________________________||
