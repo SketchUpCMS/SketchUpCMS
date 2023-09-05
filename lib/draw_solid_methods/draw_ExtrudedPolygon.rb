bbb5b3c924f0d36f9a04c0d90fe8be0f707f7d44
Thomas McCauley <tpmccauley@gmail.com>
2023-09-05 16:49:44 +0100

    extruded polygon
    * finish implementation of extruded polygon
    
    * draw side faces
    
    * closes #8

--- lib/draw_solid_methods/draw_ExtrudedPolygon.rb
+++ lib/draw_solid_methods/draw_ExtrudedPolygon.rb
@@ -21,8 +21,53 @@ def draw_ExtrudedPolygon entities, args
       face = entities.add_face points
       
     end
-
   end
+
+  
+  def makeSideFaces(a, b, entities)
+
+    vertices = Array.new
+
+    (0..a.length-1).each do |ai|
+
+      if ai < a.length-1
+        vertices = [a[ai], b[ai], b[ai+1], a[ai+1]]
+      else
+        vertices = [a[ai], b[ai], b[0], a[0]]
+      end
+
+      face = entities.add_face vertices
+      
+    end
+  end
+
+  
+  def connectFaces(entities, args)
+
+    zpoints = Array.new
+    
+    args["ZXYSection"].each do |zxy|
+
+      points = Array.new
+      
+      args["XYPoint"].each do |xy|
+
+        x = xy["x"]*zxy["scale"] + zxy["x"]
+        y = xy["y"]*zxy["scale"] + zxy["y"]
+        
+        point = Geom::Point3d.new(x, y, zxy["z"])        
+        points << point
+      end
+
+      zpoints << points
+      
+    end
+
+    (0..zpoints.length-2).each do |p|
+      makeSideFaces(zpoints[p], zpoints[p+1], entities)
+    end
+  end
+
   
   def reverseFacesIfInward(entities)
     for e in entities
@@ -35,8 +80,9 @@ def draw_ExtrudedPolygon entities, args
   group = entities.add_group
   entities = group.entities
   defineFaces entities, args
+  connectFaces entities, args
   
-  #reverseFacesIfInward(entities)
+  reverseFacesIfInward(entities)
 
   group
   
